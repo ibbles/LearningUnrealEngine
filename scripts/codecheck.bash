60975122d1f2b01d35ffc20428b0b02a8e44d2f8
Martin Nilsson <ibbles@hotmail.com>
2021-05-06 22:22:40 +0200

    Add white-space only line check to codecheck.bash

--- scripts/codecheck.bash
+++ scripts/codecheck.bash
@@ -15,3 +15,17 @@ done
 # for f in *.md
 #      sed -i 's,\t,    ,g' "$f"
 # end
+
+
+echo -e "Files with whitespace-only lines:"
+for file in *.md ; do
+	with_whitespace_only=$(grep -En "^[[:blank:]]+\$" "${file}")
+	if [ -n "${with_whitespace_only}" ] ; then
+		echo -e "  # $file #\n$with_whitespace_only"
+	fi
+done
+
+# These can be fixed with:
+# for f in *.md
+#     $(sed -i -e 's,^\s\+$,,g' "%f"
+# end
